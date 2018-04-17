6343431e3738aa3769e96d95d819a6b154101176
IlyasAkbergen <ilijas9813@gmail.com>
2018-04-17 19:40:36 +0600

    first upload
    upload

---
+++ app/src/main/java/com/backendless/examples/login_with_sdk/choose_social_network_activity.java
@@ -0,0 +1,50 @@
+package com.backendless.examples.login_with_sdk;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import com.backendless.Backendless;
+
+public class choose_social_network_activity extends Activity {
+	private ImageButton login_with_facebook_sdk;
+	private ImageButton login_with_google_plus_sdk;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_choose_social_network);
+		RelativeLayout lay = (RelativeLayout)findViewById(R.id.activity_choose_social_network);
+		lay.setBackgroundResource(R.drawable.background);
+		initUI();
+		initUIBehavior();
+
+		Backendless.initApp(getApplicationContext(), getString(R.string.backendless_AppId), getString(R.string.backendless_ApiKey));
+		Backendless.setUrl(getString(R.string.backendless_ApiHost));
+	}
+
+	private void initUI() {
+		login_with_facebook_sdk = (ImageButton) findViewById(R.id.button_loginWithFacebookSDK);
+		login_with_google_plus_sdk = (ImageButton) findViewById(R.id.button_loginWithGooglePlusSDK);
+	}
+
+	private void initUIBehavior() {
+		login_with_facebook_sdk.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(choose_social_network_activity.this, login_with_facebook_sdk_activity.class);
+				startActivity(intent);
+			}
+		});
+
+		login_with_google_plus_sdk.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(choose_social_network_activity.this, login_with_google_plus_sdk_activity.class);
+				startActivity(intent);
+			}
+		});
+	}
+}
